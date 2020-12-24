ec66ed9dc6893bef870e5bdc2783d1093ea2b8c7
Amol Telang <telangamol0909@gmail.com>
2020-12-24 11:45:29 +0530

    added gridfs and multer added apis

--- server/app.js
+++ server/app.js
@@ -1,19 +1,76 @@
-//backend
-
+//backend of filehosting project
 
 //importing packages
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-import path from 'path';
+import path, { resolve } from 'path';
 import multer from 'multer';
+import GridfsStorage from 'multer-gridfs-storage';
+import Grid from 'gridfs-stream';
+import methodOverride from 'method-override';
+
 
 //init express app
 const app=express();
 
 //connect database
-mongoose.connect()
+const mongoURI = 'mongodb+srv://admin:root123@filehosting.vj6pb.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const connect=mongoose.createConnection(mongoURI,{ useNewUrlParser: true ,useUnifiedTopology: true,useCreateIndex: true ,useFindAndModify:false});
+// const connectDB=async()=>{
+//     try {
+//             await mongoose.connect(mongoURI,{ useNewUrlParser: true ,useUnifiedTopology: true,useCreateIndex: true ,useFindAndModify:false})
+//             console.log('mongoose connected')
+//             console.log(`server running on port ${PORT}`);
+//     } catch (error) {
+//         console.error(error);
+//         process.exit(1);
+//     }
+// }
 
+
+//middlewares
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+
+//init gridfs
+let gfs;
+
+connect.once(`open`,()=>{
+    gfs=Grid(connect.db,mongoose.mongo);
+    gfs.collection(`uploads`);
+})
+
+//Create storage engine
+const storage=new GridfsStorage({
+    url:mongoURI,
+    file:(req,file)=>{
+        return new Promise((resolve,reject)=>{
+            const filename=file.originalname+path.extname(file.originalname);
+            const fileInfo={
+                filename:filename,
+                bucketName:`uploads`
+            };
+            resolve(fileInfo)
+        })
+    }
+})
+//multer upload
+const upload=multer ({storage});
+
+//@route GET /home
+//@desc home page
+app.get(`/`,(req,res)=>{
+    //home page
+})
+
+
+//@route POST /upload
+//@desc upload files
+app.post(`/upload`,upload.single('file'), (req,res)=>{
+    //upload file
+
+})
 
 //init PORT variable
 const PORT=process.env.PORT||3001
@@ -21,4 +78,4 @@ const PORT=process.env.PORT||3001
 //server listen
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`);
-})
+});
